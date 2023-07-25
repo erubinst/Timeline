0e62510c1072cf2832148c876bbc34822a1bbdf7
erubinst <erubinst@oberlin.edu>
2023-07-25 12:56:23 -0400

    using sample input file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
-from figure import fig, df, colors  # Import the df and fig from the figure module
+from figure import fig # Import the df and fig from the figure module
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -9,7 +9,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # Layout with the Gantt chart
 app.layout = html.Div([
     html.H1(children='Timeline', style={'textAlign':'center'}),
-    dcc.Graph(id='timeline-graph', figure=fig),
+    dcc.Graph(id='timeline-graph', figure=fig.plot),
     dbc.Modal(
         id='pop-up',
         children=[
@@ -17,8 +17,8 @@ app.layout = html.Div([
             dbc.ModalBody(id='pop-up-content'),
             dbc.ModalFooter(
                 [
-                    dbc.Button("Mark as Complete", id="button-complete", color="success", className="mr-2"),
-                    dbc.Button("Mark as Not Started", id="button-not-started", color="warning", className="mr-2", style={'display': 'none'}),
+                    dbc.Button("Mark as Completed", id="button-Completed", color="success", className="mr-2"),
+                    dbc.Button("Mark as Scheduled", id="button-not-started", color="warning", className="mr-2", style={'display': 'none'}),
                     dbc.Button("Cancel", id="button-cancel", color="secondary")
                 ]
             ),
@@ -35,11 +35,11 @@ app.layout = html.Div([
      Output('pop-up-content', 'children')],
     [Input('timeline-graph', 'clickData'),
      Input('button-cancel', 'n_clicks'),
-     Input('button-complete', 'n_clicks'),
+     Input('button-Completed', 'n_clicks'),
      Input('button-not-started', 'n_clicks')],
     prevent_initial_call=True
 )
-def show_or_close_pop_up(click_data, cancel_clicks, complete_clicks, not_started_clicks):
+def show_or_close_pop_up(click_data, cancel_clicks, Completed_clicks, not_started_clicks):
     ctx = dash.callback_context
     triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
@@ -47,58 +47,60 @@ def show_or_close_pop_up(click_data, cancel_clicks, complete_clicks, not_started
         task = click_data['points'][0]['y']  # Access the task name from click_data
         return True, f'You clicked on Task: {task}'
 
-    if triggered_id in ('button-cancel', 'button-complete', 'button-not-started'):
+    if triggered_id in ('button-cancel', 'button-Completed', 'button-not-started'):
         return False, None
 
     return dash.no_update, dash.no_update
 
-# Callback to update the visibility of the "Mark as Complete" and "Mark as Not Started" buttons based on the resource status
+# Callback to update the visibility of the "Mark as Completed" and "Mark as Scheduled" buttons based on the resource status
 @app.callback(
-    [Output('button-complete', 'style'),
+    [Output('button-Completed', 'style'),
      Output('button-not-started', 'style')],
     Input('timeline-graph', 'clickData'),
     prevent_initial_call=True
 )
 def update_button_visibility(click_data):
     if click_data:
-        task = click_data['points'][0]['y']  # Access the task name from click_data
-        resource_status = df.loc[df['Task'] == task, 'Resource'].values[0]
-
-        # Set the visibility of the "Mark as Complete" button based on the resource status
-        complete_button_style = {'display': 'none'} if resource_status == 'Complete' else {}
+        resource = click_data['points'][0]['y']  # Access the task name from click_data
+        task = click_data['points'][0]['customdata'][0]
+        print(click_data)
+        # need to replace search with mapping to a configuration id
+        status = fig.df.loc[(fig.df['Task'] == task) & (fig.df['Resource'] == resource), 'Status'].values[0]
+        # Set the visibility of the "Mark as Completed" button based on the resource status
+        Completed_button_style = {'display': 'none'} if status == 'Completed' else {}
         
-        # Set the visibility of the "Mark as Not Started" button based on the resource status
-        not_started_button_style = {'display': 'none'} if resource_status == 'Not Started' else {}
+        # Set the visibility of the "Mark as Scheduled" button based on the resource status
+        not_started_button_style = {'display': 'none'} if status == 'Scheduled' else {}
 
-        return complete_button_style, not_started_button_style
+        return Completed_button_style, not_started_button_style
 
     return {}, {}
 
-# Callback to update the timeline data when the "Mark as Complete" or "Mark as Not Started" button is clicked
+# Callback to update the timeline data when the "Mark as Completed" or "Mark as Scheduled" button is clicked
 @app.callback(
     Output('timeline-graph', 'figure'),
-    [Input('button-complete', 'n_clicks'),
+    [Input('button-Completed', 'n_clicks'),
      Input('button-not-started', 'n_clicks')],
     State('timeline-graph', 'figure'),
     State('timeline-graph', 'clickData'),
     prevent_initial_call=True
 )
-def update_timeline(complete_clicks, not_started_clicks, figure, click_data):
+def update_timeline(Completed_clicks, not_started_clicks, figure, click_data):
     ctx = dash.callback_context
     triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    if triggered_id in ('button-complete', 'button-not-started') and click_data:
-        task = click_data['points'][0]['y']  # Access the task name from click_data
-        if triggered_id == 'button-complete':
-            df.loc[df['Task'] == task, 'Resource'] = 'Complete'
+    if triggered_id in ('button-Completed', 'button-not-started') and click_data:
+        resource = click_data['points'][0]['y']  # Access the task name from click_data
+        print(click_data)
+        task = click_data['points'][0]['customdata'][0]
+        if triggered_id == 'button-Completed':
+            # need to replace this search with a map to a configuration id
+            fig.df.loc[(fig.df['Task'] == task) & (fig.df['Resource'] == resource), 'Status'] = 'Completed'
         elif triggered_id == 'button-not-started':
-            df.loc[df['Task'] == task, 'Resource'] = 'Not Started'
+            fig.df.loc[(fig.df['Task'] == task) & (fig.df['Resource'] == resource), 'Status'] = 'Scheduled'
 
-    # Get the unique tasks and use them to set the category_orders dynamically
-    custom_order = df['Task'].unique()
-    updated_fig = px.timeline(data_frame=df, x_start='Start', x_end='Finish', y='Task', color='Resource',
-                              custom_data=['Task'], category_orders={"Task": custom_order}, color_discrete_map=colors)
-    return updated_fig
+    fig.get_figure()
+    return fig.plot
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- figure.py
+++ figure.py
@@ -1,20 +1,37 @@
 import plotly.express as px
 import pandas as pd
+import json
 
-df = pd.DataFrame([
-    dict(Task="Job A", Start='2023-01-01 00:00', Finish='2023-01-01 01:00', Resource="Complete"),
-    dict(Task="Job B", Start='2023-01-01 04:05', Finish='2023-01-01 05:35', Resource="Not Started"),
-    dict(Task="Job C", Start='2023-01-01 00:40', Finish='2023-01-01 04:10', Resource="Not Started"),
-    dict(Task="Job D", Start='2023-01-01 00:30', Finish='2023-01-01 02:10', Resource="Complete"),
-    dict(Task="Job E", Start='2023-01-01 03:00', Finish='2023-01-01 06:10', Resource="Not Started"),
-    dict(Task="Job F", Start='2023-01-01 01:05', Finish='2023-01-01 02:35', Resource="Complete")
-])
 
-# Define the colors with RGB values corresponding to Bootstrap's "warning" and "success" colors
-colors = {'Not Started': 'rgb(255, 193, 7)',  # Yellow (Bootstrap warning color)
-          'Complete': 'rgb(40, 167, 69)'}    # Green (Bootstrap success color)
+class Figure():
+    def __init__(self, df=pd.DataFrame()):
+        self.df = df
+        self.colors = {'Scheduled': 'rgb(249,168,37)',  # Orange
+                       "Executing": "rgb(40, 167, 69)", # Green
+                       'Completed': 'rgb(0,0,0)'}   # Black
+        self.plot = None
 
-custom_order = df['Task'].unique()
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource",
-                  custom_data=['Task'], category_orders={"Task": custom_order},
-                  color_discrete_map=colors)
+    def json_to_df(self, json_file_name):
+        with open(json_file_name, "r") as json_file:
+            data = json.load(json_file)
+        self.df = pd.DataFrame(data["data"]["tasks"])
+        new_column_labels = ["lotid", "Task", "Resource",
+                             "configurationId", "Start", "Finish"]
+        self.df.columns = new_column_labels
+        new_column_data = ["Scheduled" for i in range(len(self.df))]
+        self.df.insert(loc=2, column='Status', value=new_column_data)
+        self.df['Start'] = self.df['Start'].apply(
+            lambda x: pd.to_datetime(x[0]))
+        self.df['Finish'] = self.df['Finish'].apply(
+            lambda x: pd.to_datetime(x[0]))
+
+    def get_figure(self):
+        custom_order = self.df['Resource'].unique()
+        self.plot = px.timeline(self.df, x_start="Start", x_end="Finish", y="Resource", color="Status",
+                                custom_data=['Task'], category_orders={"Resource": custom_order},
+                                color_discrete_map=self.colors, hover_name="Task")
+
+
+fig = Figure()
+fig.json_to_df("sample_inputs.json")
+fig.get_figure()
