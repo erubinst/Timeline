0bd302082858d45d4ac6e8549659e4b0ccae72dd
zackma35 <36551319+zackma35@users.noreply.github.com>
2023-11-07 12:49:17 -0500

    Update app.py to work for remote

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def update_messages():
 server.add_url_rule('/update', view_func=update_messages, methods=['POST'])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host = "0.0.0.0")
